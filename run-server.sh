5ad51a13a9af81bab9a9c0d4268a8106aafccc53
Divyesh Ramani <Divyesh.Ramani@disney.com>
2023-11-12 20:23:23 -0800

    updated script

--- run-server.sh
+++ run-server.sh
@@ -3,3 +3,5 @@ set -e # stop script in case of error at any step.
 docker build -t twilio:lex-streaming-sample .
 
 docker run -p 8889:8080 --rm -d --name twillio twilio:lex-streaming-sample
+
+rm -rf war-output/
